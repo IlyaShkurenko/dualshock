4de9355a93bfd9b30bee1f533a0d1113bd9f875b
illiashkurenko <ilya-shkurenko@mail.ru>
2017-10-30 22:41:48 +0200

    sign up function

--- src/frontend/vue-cli/src/auth.js
+++ src/frontend/vue-cli/src/auth.js
@@ -14,12 +14,28 @@ export default {
     // Send a request to the login URL and save the returned JWT
     login(context, creds, redirect) {
         context.$http.post(LOGIN_URL, creds, (data) => {
+            localStorage.setItem('id_token', data.id_token);
+            localStorage.setItem('access_token', data.access_token);
+
+            this.user.authenticated = true
+
+            // Redirect to a specified route
+            if(redirect) {
+                router.go(redirect)
+            }
+
+        }).error((err) => {
+            context.error = err
+        })
+    },
+
+    signup(context, creds, redirect) {
+        context.$http.post(SIGNUP_URL, creds, (data) => {
             localStorage.setItem('id_token', data.id_token)
             localStorage.setItem('access_token', data.access_token)
 
             this.user.authenticated = true
 
-            // Redirect to a specified route
             if(redirect) {
                 router.go(redirect)
             }
