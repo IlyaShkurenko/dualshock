ee587af2671882294a2393dad7b0d3eda15fca63
illiashkurenko <ilya-shkurenko@mail.ru>
2018-01-13 17:41:07 +0200

    production

--- src/backend/untitled/libs/mongoose.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var config = require('../config');
-mongoose.Promise = global.Promise;
-var promise = mongoose.createConnection(config.get('mongoose:uri'), {
-    useMongoClient: true,
-    /* other options */
-});
-
-module.exports = promise;
